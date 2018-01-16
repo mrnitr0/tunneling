ffff3c67aeab45bd8297c53639109aaed4fce235
burhan2601 <burhan.programers@gmail.com>
2018-01-16 10:45:41 +0700

    Update stunnel4.sh

--- stunnel4.sh
+++ stunnel4.sh
@@ -37,7 +37,7 @@ sed -i 's/#Banner/Banner/g' /etc/ssh/sshd_config
 apt-get -y install dropbear
 sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=143/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_BANNER=/DROPBEAR_BANNER="/etc/issue.net"/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_BANNER=""/DROPBEAR_BANNER="\/etc\/issue.net"/g' /etc/default/dropbear
 echo "/bin/false" >> /etc/shells
 service dropbear restart
 
