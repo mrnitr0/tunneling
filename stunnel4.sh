c014e5cf3b0d29f92744c1e93edef9235231d0b5
burhan2601 <burhan.programers@gmail.com>
2018-01-16 10:18:26 +0700

    Update stunnel4.sh

--- stunnel4.sh
+++ stunnel4.sh
@@ -35,12 +35,10 @@ cat - /etc/ssh/sshd_config <<-END
 Banner /etc/issue.net
 END
 
-#instalasi dropbear
-apt-get install dropbear -y
+apt-get -y install dropbear
 sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_PORT=/DROPBEAR_PORT=143' /etc/default/dropbear
-sed -i 's/DROPBEAR_BANNER=/DROPBEAR_BANNER="/etc/issue.net"' /etc/default/dropbear
-service dropbear start
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=443/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 109 -p 110"/g' /etc/default/dropbear
 echo "/bin/false" >> /etc/shells
 service dropbear restart
 
